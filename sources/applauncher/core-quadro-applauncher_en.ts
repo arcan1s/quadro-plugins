389d6c5a04101aa4a5743e7e1daae189d2d6b646
arcan1s <darkarcanis@mail.ru>
2015-12-13 21:38:58 +0300

    update plugins to recent changes

---
+++ sources/applauncher/core-quadro-applauncher_en.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AppLauncher</name>
+    <message>
+        <location filename="applauncher.cpp" line="56"/>
+        <source>Launcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="applauncher.cpp" line="134"/>
+        <source>Type for search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="applauncher.cpp" line="208"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="applauncher.cpp" line="209"/>
+        <source>Could not run application %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ sources/applauncher/core-quadro-applauncher_ru.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AppLauncher</name>
+    <message>
+        <location filename="applauncher.cpp" line="56"/>
+        <source>Launcher</source>
+        <translation>Лаунчер</translation>
+    </message>
+    <message>
+        <location filename="applauncher.cpp" line="134"/>
+        <source>Type for search</source>
+        <translation>Введите для поиска</translation>
+    </message>
+    <message>
+        <location filename="applauncher.cpp" line="208"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="applauncher.cpp" line="209"/>
+        <source>Could not run application %1</source>
+        <translation>Не могу запустить приложение %1</translation>
+    </message>
+</context>
+</TS>
